c143bc3e00c08500db9a4e626e31525cd7ed140d
Yurkovich <yurkov200000@gmail.com>
2025-04-26 13:55:12 +0700

    Smooth scroll

--- js/main.js
+++ js/main.js
@@ -45,3 +45,18 @@ function updateKrasnoyarskTime() {
 updateKrasnoyarskTime();
 
 setInterval(updateKrasnoyarskTime, 60000);
+
+// Скролл
+
+SmoothScroll({
+    animationTime: 800,
+    stepSize: 75,
+    accelerationDelta: 30,
+    accelerationMax: 2,
+    keyboardSupport: true,
+    arrowScroll: 50,
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+    touchpadSupport: true,
+})
